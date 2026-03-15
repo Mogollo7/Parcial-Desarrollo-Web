e26b99580931e0c51238bf16eeaf85387228d2c7
Brandsete <usmabrandsamuel@gmail.com>
2026-03-14 19:12:57 -0500

    Implementacion Token no enviado y Token expirado
    se implementaron mensajes los cuales se encargan de notificar al usuario si el token que ha usado no se envió o cuando el tiempo estipulado se ha acabado
    
    Colaborador del cambio: Cesar-csr

--- helpers/auth.js
+++ helpers/auth.js
@@ -11,19 +11,32 @@ function createToken(usuario) {
         email: usuario.email,
         role: usuario.role,
         iat: moment().unix(),
-        exp: moment().add(5, 'minutes').unix()
+        exp: moment().add(15, 'minutes').unix()
     };
     return jwt.encode(payload, secret);
 }
 
 function validateToken(req, res, next) {
+    
+    if(!req.headers.authorization){
+        return res.status(403).send({message: 'No se envió token'});
+    }
+
     try {
-        let token = req.headers.authorization.replace(' Bearer ', '');
+
+        let token = req.headers.authorization.replace('Bearer ', '');
         let payload = jwt.decode(token, secret);
+
+        if(payload.exp <= moment().unix()){
+            return res.status(401).send({message: 'Token expirado'});
+        }
+
         req.user = payload;
         next();
-    }catch(ex) {
-        res.status(401).send({message: 'Token no válido'}); }
 
-    };
+    } catch(ex) {
+        return res.status(401).send({message: 'Token no válido'});
+    }
+
+}
 module.exports = {createToken, validateToken};
